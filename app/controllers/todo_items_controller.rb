49ec7f115bafad43a631f52f9f797a4b9917147c
Richard Moss <richard@englishtutorslive.com>
2016-04-17 19:51:24 +0200

    Shows number of completed todo_items on index page, passes all remaining tests

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -5,6 +5,7 @@ class TodoItemsController < ApplicationController
   # GET /todo_items.json
   def index
     @todo_items = TodoItem.all
+    @number_of_completed_todos = TodoItem.done_todo
   end
 
   # GET /todo_items/1
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,2 +1,5 @@
 class TodoItem < ActiveRecord::Base
+  def self.done_todo
+    TodoItem.where(completed: true).to_a.count
+  end
 end
